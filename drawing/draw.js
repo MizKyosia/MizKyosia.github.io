8a8ca6632ec6186b26dc716068c5650b5a37a18a
MizKyosia <mizkyosia@gmail.com>
2023-05-22 11:05:01 +0200

    Version 1.0 (color edit, add, delete and select)

--- drawing/draw.js
+++ drawing/draw.js
@@ -1,32 +1,47 @@
 /** @type {CanvasRenderingContext2D} */
 let ctx;
-var color = -1;
-let id = 0;
+/** @type {CanvasRenderingContext2D} */
+let pctx;
+/** @type {HTMLImageElement} */
+var cursor, color = -1, previousPos, previousColor, startDrawPos, pen = 0, colorPicker, editedColor, colorOption = 0;
 document.addEventListener('DOMContentLoaded', () => {
     ctx = document.getElementById('drawing').getContext('2d');
-    document.addEventListener('keydown', (e) => document.getElementById('colors').innerHTML += `<div class="color" style="background-color: ${randomHexColor()};">`);
+    pctx = document.getElementById('drawingPreview').getContext('2d');
+    cursor = document.getElementById('cursor');
+    colorPicker = document.getElementById('colorPicker');
+    changePenSize(5)
+    resizeCanvas();
+    ctx.lineCap = 'round';
+    document.getElementById('addColor').addEventListener('click', (e) => addColor(e));
     document.addEventListener('auxclick', (e) => false);
     document.addEventListener('contextmenu', (e) => {
-        if (e.target.classList.contains('color') || e.target.classList.contains('selectedColor') || e.target.id == 'drawing') {
+        if (e.target.classList.contains('color') || e.target.classList.contains('selectedColor') || e.target.id.includes('drawing')) {
             e.preventDefault();
             return false;
         }
     });
     document.addEventListener('mousedown', (e) => {
-        if (e.target.classList.contains('color')||e.target.classList.contains('selectedColor')) return selectColor(e);
-        else if (e.target.id == 'drawing'){
+        if (e.target.id != 'colorPicker' && editedColor) stopEdit();
+        if (e.target.classList.contains('color') || e.target.classList.contains('selectedColor')) return colorAction(e);
+        else if (e.target.id.includes('drawing')) {
             e.preventDefault();
             color = e.button;
+            if (pen == Pens.base) drawBase(e);
             return false;
         };
     });
     document.addEventListener('mouseup', (e) => {
-        if (e.target.id == 'drawing'){
-            color = -1;
-            return false;
-        }
+        previousPos = null;
+        color = -1;
+        return false;
     });
-    ctx.canvas.addEventListener('mousemove', async (e) => color >= 0 ? await draw(e) : false);
+    ctx.canvas.addEventListener('mouseenter', (e) => cursor.parentElement.style.display = '');
+    ctx.canvas.addEventListener('mouseleave', (e) => cursor.parentElement.style.display = 'none');
+    ctx.canvas.addEventListener('mousemove', (e) => {
+        cursor.parentElement.style.left = `${e.pageX}px`;
+        cursor.parentElement.style.top = `${e.pageY}px`;
+        if (color >= 0) draw(e);
+    });
 })
 
 /**
@@ -58,6 +73,61 @@ function fullScreen() {
     else ctx.canvas.parentElement.requestFullscreen();
 }
 
+function addColor(e) {
+    if (editedColor) stopEdit();
+    var a = document.createElement('button');
+    a.style = 'background-color: black; box-shadow: inset 0 0 0 50px black;';
+    a.className = 'color';
+    document.getElementById('colors').appendChild(a);
+    startEdit(e,a);
+}
+
+function offset(el) {
+    var rect = el.getBoundingClientRect(),
+        scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+        scrollTop = window.scrollY || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
+
+function stopEdit() {
+    editedColor.classList.remove('editing');
+    editedColor = null;
+}
+
+function editColor(c) {
+    if (!editedColor) return;
+    editedColor.style = `background-color: ${c}; box-shadow: inset 0 0 0 50px ${c}`;
+}
+
+//#region penSettings
+
+function changeOption(e,o){
+    colorOption = o;
+    document.getElementById('colors').className = `action${colorOption}`;
+    document.querySelector('.colorOption.selected').classList.remove('selected');
+    e.classList.add('selected');
+    console.log(e,colorOption);
+}
+
+function startEdit(e,a){
+    editedColor = a;
+    editedColor.classList.add('editing');
+    var o = offset(e.target);
+    colorPicker.style.left = `${o.left}px`;
+    colorPicker.style.top = `${o.top}px`;
+    colorPicker.click();
+}
+
+function colorAction(e){
+    console.log(colorOption);
+    switch(colorOption) {
+        case 0: return selectColor(e);
+        case 1: return startEdit(e,e.target);
+        case 2: return e.target.remove();
+    }
+    return;
+}
+
 /**
  * @param {MouseEvent} e
  */
@@ -79,34 +149,104 @@ function selectColor(e) {
 }
 
 /**
- * @param {MouseEvent} e 
+ * @param {string} p 
  */
-async function draw(e) {
-    let t = Date.now();
-    const c = document.querySelector(`.color${color}`)?.style.backgroundColor;
-    if (!c) return false;
-    let pos = getMousePos(e);
-    ctx.beginPath()
-    ctx.fillStyle = c;
-    ctx.arc(pos[0],pos[1],5,0,2*Math.PI)
-    ctx.fill()
-    ctx.closePath()
-    console.log(Date.now(),t,(Date.now() - t))
+function changePen(p) {
+    pen = Pens[p];
+}
+
+/**
+ * @param {number} v 
+ */
+function changePenSize(v) {
+    ctx.lineWidth = v;
+    for (var e of document.querySelectorAll('.shape')) e.style.strokeWidth = v;
+    cursor.setAttribute('r', v / 2);
+}
+
+function changeShapes(f) {
+    console.log(f);
+    for (var e of document.querySelectorAll('.shape')) (f && e.id != 'line') ? e.classList.remove('hollow') : e.classList.add('hollow');
+}
+//#endregion
+
+/*setInterval(() => {
+    console.log("update")
+
+}, 1)*/
+
+//#region drawing
+
+function draw(e) {
+    switch (pen) {
+        case 0: return drawBase(e);
+        case 1: return drawRectangle(e)
+    }
+    console.log('problem')
     return false;
 }
 
 /**
  * @param {MouseEvent} e 
+ */
+function drawBase(e) {
+    const c = document.querySelector(`.color${color}`)?.style.backgroundColor;
+    if (!c) return false;
+    if (ctx.fillStyle != c) {
+        ctx.fillStyle = c;
+        ctx.strokeStyle = c;
+    }
+    ctx.beginPath();
+    let pos = getMousePos(e);
+    if (previousPos) {
+        ctx.moveTo(pos[0], pos[1])
+        ctx.lineTo(previousPos[0], previousPos[1])
+    }
+    ctx.stroke()
+    previousPos = pos;
+    return false;
+}
+
+/**
+ * @param {MouseEvent} e 
+ */
+function drawRectangle(e) {
+    pctx.clearRect();
+    if (!startDrawPos) startDrawPos = [e.x, e.y];
+    pctx.fillRect(e.x, e.y, startDrawPos[0] - e.x, startDrawPos[1] - e.y);
+    return false;
+}
+//#endregion
+
+/**
+ * @param {MouseEvent} e 
  * @returns {[number,number]} Position in [x,y] coordinates
  */
-function getMousePos(e){
+function getMousePos(e) {
     var rect = e.target.getBoundingClientRect();
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
-    return [Math.round(x),Math.round(y)];
+    return [Math.round(x), Math.round(y)];
 }
 
-function download(){
+function download() {
     document.getElementById('download').href = ctx.canvas.toDataURL();
     document.getElementById('download').click();
+}
+
+function resizeCanvas() {
+    pctx.canvas.width = ctx.canvas.width;
+    pctx.canvas.height = ctx.canvas.height;
+}
+
+function clearCanvas() {
+    pctx.clearRect(0, 0, pctx.canvas.width, pctx.canvas.height);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+const Pens = {
+    "base": 0,
+    "rectangle": 1,
+    "circle": 2,
+    "fill": 3
 }
